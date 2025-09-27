b9e333a597fd6cda8f92bd9112893aba2814a417
buithanhthien24 <thienbt03@gmail.com>
2025-09-27 10:19:47 +0700

    init be

---
+++ FamilyKitchenHub/src/main/java/com/c2se04/familykitchenhub/FamilyKitchenHubApplication.java
@@ -0,0 +1,13 @@
+package com.c2se04.familykitchenhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyKitchenHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyKitchenHubApplication.class, args);
+	}
+
+}
---
+++ FamilyKitchenHub/src/test/java/com/c2se04/familykitchenhub/FamilyKitchenHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c2se04.familykitchenhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyKitchenHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
